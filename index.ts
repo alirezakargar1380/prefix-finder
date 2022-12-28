ca7ef6704ed61845be6c12f34f87f23a52a7d5e6
alireza kargar <imanesche@gmail.com>
2022-12-28 16:07:18 +0330

    added findAllPrefix

--- index.ts
+++ index.ts
@@ -2,9 +2,10 @@ interface IPrefix {
     strs: Array<string>
     isPrefix(): boolean
     findPrefix(): string
+    findAllPrefix(): PrefixReport
 }
 
-class prefix implements IPrefix {
+export default class prefix implements IPrefix {
     public strs: Array<string>
     constructor(strs: Array<string>) {
         this.strs = strs
@@ -28,5 +29,40 @@ class prefix implements IPrefix {
             str0 = str0.slice(0, i)
         }
         return str0
+    }
+
+    findAllPrefix(): PrefixReport {
+        let strs = [...this.strs]
+
+        // sort strings by english alphabet
+        strs.sort((a, b) => a.localeCompare(b))
+
+        let str0: string = strs[0]
+        let map: PrefixReport = {}
+        let array: string[] = []
+
+        for (let char of strs) {
+            let i = 0
+            if (char[i] !== str0[i]) {
+                map[str0] = {
+                    prefix: str0,
+                    array: array
+                }
+
+                array = []
+                str0 = char
+                array.push(char)
+            } else array.push(char)
+
+            while (str0.length > i && char[i] === str0[i]) { i++ }
+            str0 = str0.slice(0, i)
+        }
+
+        map[str0] = {
+            prefix: str0,
+            array: array
+        }
+
+        return map
     }
 }
