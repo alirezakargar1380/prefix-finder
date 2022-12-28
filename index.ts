d2b2278a28383519b92ebd187d77fe82e3717591
alireza kargar <imanesche@gmail.com>
2022-12-28 16:05:34 +0330

    added isPrefix

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
 interface IPrefix {
     strs: Array<string>
+    isPrefix(): boolean
 }
 
 class prefix implements IPrefix {
     public strs: Array<string>
     constructor(strs: Array<string>) {
         this.strs = strs
+    }
+
+    isPrefix(): boolean {
+        let str0: string = this.strs[0]
+        for (let char of this.strs) {
+            let i = 0
+            while (str0.length > i && char[i] === str0[i]) { i++ }
+            str0 = str0.slice(0, i)
+        }
+        return (str0) ? true : false
     }
 }
